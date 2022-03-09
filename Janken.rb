183b7a6c4a560282f4ab98688a7de8591d06c292
yuka-52 <98066974+yuka-52@users.noreply.github.com>
2022-03-09 14:53:51 +0900

    Update janken

--- Janken.rb
+++ Janken.rb
@@ -1,90 +1,83 @@
-puts "じゃんけん、、、"
-
+class Jankengame
+    
 def initialize
-  @result = ""
+ @result = ""
 end
-
-def janken_time
+    
+puts "じゃんけん"
+def janken
   puts "0(グー)1(チョキ)2(パー)3(戦わない)"
   player_hand = gets.to_i
   program_hand = rand(3)
-  jankens = ["グー","チョキ","パー"]
+  jankens = ["グー", "チョキ", "パー"]
   puts "ホイ！"
-  puts "------------"
-  puts "あなた:#{jankens[player_hand]}を出しました"
-  puts "相手:#{jankens[program_hand]}を出しました"
-  puts "------------"
+  puts "---------------"
+  puts "あなた：#{jankens[player_hand]}を出しました"
+  puts "相手  ：#{jankens[program_hand]}を出しました"
 
-win =(player_hand == 0 && program_hand == 1) || (player_hand == 1 && player_hand ==  2) || (player_hand == 2 && program_hand == 0)
-lose =!(win)
-
-
-if player_hand == program_hand
+win = (player_hand == 0 && program_hand == 1) || (player_hand == 1 && program_hand == 2) || (player_hand == 2 && program_hand == 0)
+draw = player_hand == program_hand
+        
+if draw
+  puts "---------------"
   puts "あいこで"
-  puts "ショ！"
-  return true
+  @result = "draw"
 elsif win
   puts "勝ちました！"
+  puts "---------------"
   @result = "win"
-elsif
+else
   puts "負けました！"
+  puts "---------------"
   @result = "lose"
 end
-end
-
-next_game = true
-while next_game
-  next_game = janken_time
-end
-
-def hoi
-if @result == "win"
-  puts "あっち向いて〜"
-  puts "0(上)1(下)2(左)3(右)"
-  player_finger == gets.to_i
-  program_face == rand(4)
-  hois =["上","下","右","左"]
-  puts "ホイ！"
-  puts "------------"
-  puts "あなた:#{hois [player_finger]}"
-  puts "相手:#{hois [program_face]}"
-  puts "------------"
-
-  if player_finger == program_face
-    puts "あなたの勝ち！"
-  elsif player_finger != program_face
-    puts "もう一回じゃんけんし直し"
-    return true
-    end
-end
-end
-
-if @result == 'lose'
-    puts "あっち向いて〜"
-    puts "0(上)1(下)2(左)3(右)"
     
-    player_face = gets.to_i
-    program_finger = rand(4)
-
-    hois =["上","下","右","左"]
-    puts "ホイ！"
-    puts "------------"
-    puts "あなた:#{hois [player_finger]}"
-    puts "相手:#{hois [program_face]}"
-    puts "------------"
-
+if @result == "draw"
+  janken
+elsif @result == "win"
+  puts "あっち向いて〜"
+  puts "0(上) 1(下) 2(右) 3(左)"
+  player_finger = gets.to_i
+  program_face = rand(4)
+  hoi = ["上", "下", "右", "左"]
+  puts "ホイ"
+  puts "---------------"
+  if player_finger == program_face
+    puts "あなた：#{hoi[player_finger]}"
+    puts "相手  ：#{hoi[program_face]}"
+    puts "勝ちました！"
+    puts "---------------"
+  else
+    puts "あなた：#{hoi[player_finger]}"
+    puts "相手  ：#{hoi[program_face]}"
+    puts "残念！"
+    puts "---------------"
+    janken
+    end
+elsif @result == "lose"
+  puts "相手：あっち向いて〜"
+  puts "あなた：0(上) 1(下) 2(右) 3(左)"
+  player_face= gets.to_i
+  program_finger = rand(4)
+  hoi= ["上", "下", "右", "左"]
+  puts "ホイ"
+  puts "---------------"
   if player_face == program_finger
-    puts "あなたの負け"
-  elsif player_face != program_finger
-    puts "もう一回じゃんけんし直し"
-    janken_time
+    puts "あなた：#{hoi[player_face]}"
+    puts "相手  ：#{hoi[program_finger]}"
+    puts "あなたの負けです"
+    puts "---------------"
+　　else
+　　　　puts "あなた：#{hoi[player_face]}"
+　　　　puts "相手  ：#{hoi[program_finger]}"
+　　　　puts "▶︎勝敗つかず"
+　　　　puts "---------------"
+    　end
+    end
     end
 end
 
-next_game = true
-while next_game
-    next_game = janken_time
-end
+jankengame = Jankengame.new
+jankengame.janken
 
-janken_time
-hoi
+end
